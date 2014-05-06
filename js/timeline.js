99bb45404dd509f23fd84c7fed26f471a1bd21a9
ZeningQu <quzening@gmail.com>
2014-05-06 02:16:37 -0700

    changed x axis to time;

--- js/timeline.js
+++ js/timeline.js
@@ -1,18 +1,22 @@
 // JS for timeline view
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 1084 - margin.left - margin.right,
+    width = 500 - margin.left - margin.right,
     height = 200 - margin.top - margin.bottom;
+var columnwidth = 15;
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
+var x = d3.time.scale()
+    .range([0, width]);
 
 var y = d3.scale.linear()
     .range([height, 0]);
 
+var parse = d3.time.format("%Y-%m-%d").parse;
+
 var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("bottom");
+    .orient("bottom")
+    .ticks(2);
 
 var yAxis = d3.svg.axis()
     .scale(y)
@@ -31,7 +35,7 @@ var svg = d3.select("#timeline").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.tsv("js/timeline_data.tsv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.date; }));
+  x.domain([data[data.length-1].date, data[0].date]);
   y.domain([ (-1) * d3.max(data, function(d) { return d.outgoing; }), d3.max(data, function(d) { return d.incoming; })]);
 
   svg.append("g")
@@ -72,14 +76,14 @@ y_render.append("text")
         bargroup.append("rect")
         .attr("class", "inbar")
         .attr("x", function(d) { return x(d.date); })
-        .attr("width", x.rangeBand())
+        .attr("width", columnwidth)
         .attr("y", function(d) { return y(d.incoming); })
         .attr("height", function(d) { return y(0) - y(d.incoming); });
 
         bargroup.append("rect")
         .attr("class", "outbar")
         .attr("x", function(d) { return x(d.date); })
-        .attr("width", x.rangeBand())
+        .attr("width", columnwidth)
         .attr("y", function(d) { return y(0); })
         .attr("height", function(d) { return y( (-1)*d.outgoing) - y(0); });
       });
@@ -101,6 +105,7 @@ function drawbars(selection)
 }
 
 function type(d) {
+  d.date = parse(d.date);
   d.incoming = +d.incoming;
   return d;
 }
