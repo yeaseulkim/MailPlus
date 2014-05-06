0a3cffc5cce5a8a387549e4a30f5d02b1b96708a
ZeningQu <quzening@gmail.com>
2014-05-06 01:08:29 -0700

    added incoming and outgoing bars;

--- js/timeline.js
+++ js/timeline.js
@@ -1,8 +1,8 @@
 // JS for timeline view
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 1084 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
@@ -17,7 +17,7 @@ var xAxis = d3.svg.axis()
 var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left")
-    .ticks(10, "%");
+    .ticks(5);
 
 var svg = d3.select("#timeline").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -26,36 +26,72 @@ var svg = d3.select("#timeline").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.tsv("js/timeline_data.tsv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.letter; }));
-  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+  x.domain(data.map(function(d) { return d.date; }));
+  y.domain([ (-1) * d3.max(data, function(d) { return d.outgoing; }), d3.max(data, function(d) { return d.incoming; })]);
 
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
 
-  svg.append("g")
+var y_render = svg.append("g")
       .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
+      .call(yAxis);
+
+y_render.append("text")
+      //.attr("transform", "rotate(-90)")
+      .attr("x", 6)
+      .attr("y", 0)
       .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Frequency");
+      .style("text-anchor", "start")
+      .text("incoming");
+
+y_render.append("text")
+      //.attr("transform", "rotate(-90)")
+      .attr("x", 6)
+      .attr("y", height)
+      .attr("dy", ".71em")
+      .style("text-anchor", "start")
+      .text("outgoing");
 
   svg.selectAll(".bar")
       .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.letter); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.frequency); })
-      .attr("height", function(d) { return height - y(d.frequency); });
+    .enter()
+    .call(function()
+      {
+        var bargroup = this.append("g")
+        bargroup.append("rect")
+        .attr("class", "inbar")
+        .attr("x", function(d) { return x(d.date); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.incoming); })
+        .attr("height", function(d) { return y(0) - y(d.incoming); });
+
+        bargroup.append("rect")
+        .attr("class", "outbar")
+        .attr("x", function(d) { return x(d.date); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(0); })
+        .attr("height", function(d) { return y( (-1)*d.outgoing) - y(0); });
+      });
+
+
+    
+    
 
 });
 
+function drawbars(selection)
+{
+  selection.append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.date); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.incoming); })
+      .attr("height", function(d) { return y(0) - y(d.incoming); });
+}
+
 function type(d) {
-  d.frequency = +d.frequency;
+  d.incoming = +d.incoming;
   return d;
 }
