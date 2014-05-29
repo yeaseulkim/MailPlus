f50e5259fab11b3f90e46a4d382e4a5dea6d6b05
Jessie X <jessiex95@gmail.com>
2014-05-28 22:35:42 -0700

    complete topic interaction complete

--- js/topic.js
+++ js/topic.js
@@ -3,4 +3,16 @@ $(function() {
 		collapsible: true,
 		active:false
     });
+
+	$(".open").click(function() {
+        $("#newemail").html("<img src='./imgs/newemail.png' >");
+        $("#newemail").dialog();
+        $("#newemail").dialog("option", "width", 560);
+        $("#newemail").dialog("option", "height", 350);
+        $("#newemail").dialog("widget").position({
+    		of: $(this).parent(),
+    		my: "right top",
+    		at: "left top"
+  		});
+    });
 });
